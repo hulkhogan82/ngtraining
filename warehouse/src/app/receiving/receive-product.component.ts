7e5bf79738b81ed98ec9e03e9a05b75c3385f744
Hulk Hogan <atm1982@yahoo.com>
2020-10-07 23:47:30 -0400

    Lab 5 components completed

--- warehouse/src/app/receiving/receive-product.component.ts
+++ warehouse/src/app/receiving/receive-product.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'nw-receive-product',
   templateUrl: './receive-product.component.html',
-  styleUrls: ['./receive-product.component.css']
+  styleUrls: ['./receive-product.component.css'],
+  styles: ['#orderContents img { height: 50px }']
 })
 export class ReceiveProductComponent implements OnInit {
 
